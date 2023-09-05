3165ae1842d21cda7990726c35703a3e36016990
oliverc1623 <oliverc1622@gmail.com>
2023-09-05 17:56:31 +0000

    think I got MLPPolicySL update working

--- hw1/cs285/policies/MLP_policy.py
+++ hw1/cs285/policies/MLP_policy.py
@@ -81,7 +81,9 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
             observation = obs[None]
 
         # TODO return the action that the policy prescribes
-        raise NotImplementedError
+        torch_obs = torch.from_numpy(obs)
+        new_actions = self.forward(torch_obs)
+        return new_actions
 
     # update/train this policy
     def update(self, observations, actions, **kwargs):
@@ -93,7 +95,8 @@ class MLPPolicy(BasePolicy, nn.Module, metaclass=abc.ABCMeta):
     # return more flexible objects, such as a
     # `torch.distributions.Distribution` object. It's up to you!
     def forward(self, observation: torch.FloatTensor) -> Any:
-        raise NotImplementedError
+        new_actions = self.mean_net(observation.to(ptu.device))
+        return new_actions
 
 
 #####################################################
@@ -109,7 +112,8 @@ class MLPPolicySL(MLPPolicy):
             adv_n=None, acs_labels_na=None, qvals=None
     ):
         # TODO: update the policy and return the loss
-        loss = TODO
+        new_actions = self.get_action(observations)
+        loss = self.loss(new_actions, torch.from_numpy(actions).to(ptu.device))
         return {
             # You can add extra logging information here, but keep this line
             'Training Loss': ptu.to_numpy(loss),
